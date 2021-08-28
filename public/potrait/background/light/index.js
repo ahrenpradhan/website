f943fc4f1665c23d16bfd30fec6da76179c885db
ahrenpradhan <ahrenpradhan@gmail.com>
2021-08-28 23:29:36 +0530

    images and video

---
+++ public/potrait/background/light/index.js
@@ -0,0 +1,361 @@
+import Image_1 from './light_1.png'
+import Image_2 from './light_2.png'
+import Image_3 from './light_3.png'
+import Image_4 from './light_4.png'
+import Image_5 from './light_5.png'
+import Image_6 from './light_6.png'
+import Image_7 from './light_7.png'
+import Image_8 from './light_8.png'
+import Image_9 from './light_9.png'
+import Image_10 from './light_10.png'
+import Image_11 from './light_11.png'
+import Image_12 from './light_12.png'
+import Image_13 from './light_13.png'
+import Image_14 from './light_14.png'
+import Image_15 from './light_15.png'
+import Image_16 from './light_16.png'
+import Image_17 from './light_17.png'
+import Image_18 from './light_18.png'
+import Image_19 from './light_19.png'
+import Image_20 from './light_20.png'
+import Image_21 from './light_21.png'
+import Image_22 from './light_22.png'
+import Image_23 from './light_23.png'
+import Image_24 from './light_24.png'
+import Image_25 from './light_25.png'
+import Image_26 from './light_26.png'
+import Image_27 from './light_27.png'
+import Image_28 from './light_28.png'
+import Image_29 from './light_29.png'
+import Image_30 from './light_30.png'
+import Image_31 from './light_31.png'
+import Image_32 from './light_32.png'
+import Image_33 from './light_33.png'
+import Image_34 from './light_34.png'
+import Image_35 from './light_35.png'
+import Image_36 from './light_36.png'
+import Image_37 from './light_37.png'
+import Image_38 from './light_38.png'
+import Image_39 from './light_39.png'
+import Image_40 from './light_40.png'
+import Image_41 from './light_41.png'
+import Image_42 from './light_42.png'
+import Image_43 from './light_43.png'
+import Image_44 from './light_44.png'
+import Image_45 from './light_45.png'
+import Image_46 from './light_46.png'
+import Image_47 from './light_47.png'
+import Image_48 from './light_48.png'
+import Image_49 from './light_49.png'
+import Image_50 from './light_50.png'
+import Image_51 from './light_51.png'
+import Image_52 from './light_52.png'
+import Image_53 from './light_53.png'
+import Image_54 from './light_54.png'
+import Image_55 from './light_55.png'
+import Image_56 from './light_56.png'
+import Image_57 from './light_57.png'
+import Image_58 from './light_58.png'
+import Image_59 from './light_59.png'
+import Image_60 from './light_60.png'
+import Image_61 from './light_61.png'
+import Image_62 from './light_62.png'
+import Image_63 from './light_63.png'
+import Image_64 from './light_64.png'
+import Image_65 from './light_65.png'
+import Image_66 from './light_66.png'
+import Image_67 from './light_67.png'
+import Image_68 from './light_68.png'
+import Image_69 from './light_69.png'
+import Image_70 from './light_70.png'
+import Image_71 from './light_71.png'
+import Image_72 from './light_72.png'
+import Image_73 from './light_73.png'
+import Image_74 from './light_74.png'
+import Image_75 from './light_75.png'
+import Image_76 from './light_76.png'
+import Image_77 from './light_77.png'
+import Image_78 from './light_78.png'
+import Image_79 from './light_79.png'
+import Image_80 from './light_80.png'
+import Image_81 from './light_81.png'
+import Image_82 from './light_82.png'
+import Image_83 from './light_83.png'
+import Image_84 from './light_84.png'
+import Image_85 from './light_85.png'
+import Image_86 from './light_86.png'
+import Image_87 from './light_87.png'
+import Image_88 from './light_88.png'
+import Image_89 from './light_89.png'
+import Image_90 from './light_90.png'
+import Image_91 from './light_91.png'
+import Image_92 from './light_92.png'
+import Image_93 from './light_93.png'
+import Image_94 from './light_94.png'
+import Image_95 from './light_95.png'
+import Image_96 from './light_96.png'
+import Image_97 from './light_97.png'
+import Image_98 from './light_98.png'
+import Image_99 from './light_99.png'
+import Image_100 from './light_100.png'
+import Image_101 from './light_101.png'
+import Image_102 from './light_102.png'
+import Image_103 from './light_103.png'
+import Image_104 from './light_104.png'
+import Image_105 from './light_105.png'
+import Image_106 from './light_106.png'
+import Image_107 from './light_107.png'
+import Image_108 from './light_108.png'
+import Image_109 from './light_109.png'
+import Image_110 from './light_110.png'
+import Image_111 from './light_111.png'
+import Image_112 from './light_112.png'
+import Image_113 from './light_113.png'
+import Image_114 from './light_114.png'
+import Image_115 from './light_115.png'
+import Image_116 from './light_116.png'
+import Image_117 from './light_117.png'
+import Image_118 from './light_118.png'
+import Image_119 from './light_119.png'
+import Image_120 from './light_120.png'
+import Image_121 from './light_121.png'
+import Image_122 from './light_122.png'
+import Image_123 from './light_123.png'
+import Image_124 from './light_124.png'
+import Image_125 from './light_125.png'
+import Image_126 from './light_126.png'
+import Image_127 from './light_127.png'
+import Image_128 from './light_128.png'
+import Image_129 from './light_129.png'
+import Image_130 from './light_130.png'
+import Image_131 from './light_131.png'
+import Image_132 from './light_132.png'
+import Image_133 from './light_133.png'
+import Image_134 from './light_134.png'
+import Image_135 from './light_135.png'
+import Image_136 from './light_136.png'
+import Image_137 from './light_137.png'
+import Image_138 from './light_138.png'
+import Image_139 from './light_139.png'
+import Image_140 from './light_140.png'
+import Image_141 from './light_141.png'
+import Image_142 from './light_142.png'
+import Image_143 from './light_143.png'
+import Image_144 from './light_144.png'
+import Image_145 from './light_145.png'
+import Image_146 from './light_146.png'
+import Image_147 from './light_147.png'
+import Image_148 from './light_148.png'
+import Image_149 from './light_149.png'
+import Image_150 from './light_150.png'
+import Image_151 from './light_151.png'
+import Image_152 from './light_152.png'
+import Image_153 from './light_153.png'
+import Image_154 from './light_154.png'
+import Image_155 from './light_155.png'
+import Image_156 from './light_156.png'
+import Image_157 from './light_157.png'
+import Image_158 from './light_158.png'
+import Image_159 from './light_159.png'
+import Image_160 from './light_160.png'
+import Image_161 from './light_161.png'
+import Image_162 from './light_162.png'
+import Image_163 from './light_163.png'
+import Image_164 from './light_164.png'
+import Image_165 from './light_165.png'
+import Image_166 from './light_166.png'
+import Image_167 from './light_167.png'
+import Image_168 from './light_168.png'
+import Image_169 from './light_169.png'
+import Image_170 from './light_170.png'
+import Image_171 from './light_171.png'
+import Image_172 from './light_172.png'
+import Image_173 from './light_173.png'
+import Image_174 from './light_174.png'
+import Image_175 from './light_175.png'
+import Image_176 from './light_176.png'
+import Image_177 from './light_177.png'
+import Image_178 from './light_178.png'
+import Image_179 from './light_179.png'
+
+export const Images = Array.of(
+    Image_1,
+    Image_2,
+    Image_3,
+    Image_4,
+    Image_5,
+    Image_6,
+    Image_7,
+    Image_8,
+    Image_9,
+    Image_10,
+    Image_11,
+    Image_12,
+    Image_13,
+    Image_14,
+    Image_15,
+    Image_16,
+    Image_17,
+    Image_18,
+    Image_19,
+    Image_20,
+    Image_21,
+    Image_22,
+    Image_23,
+    Image_24,
+    Image_25,
+    Image_26,
+    Image_27,
+    Image_28,
+    Image_29,
+    Image_30,
+    Image_31,
+    Image_32,
+    Image_33,
+    Image_34,
+    Image_35,
+    Image_36,
+    Image_37,
+    Image_38,
+    Image_39,
+    Image_40,
+    Image_41,
+    Image_42,
+    Image_43,
+    Image_44,
+    Image_45,
+    Image_46,
+    Image_47,
+    Image_48,
+    Image_49,
+    Image_50,
+    Image_51,
+    Image_52,
+    Image_53,
+    Image_54,
+    Image_55,
+    Image_56,
+    Image_57,
+    Image_58,
+    Image_59,
+    Image_60,
+    Image_61,
+    Image_62,
+    Image_63,
+    Image_64,
+    Image_65,
+    Image_66,
+    Image_67,
+    Image_68,
+    Image_69,
+    Image_70,
+    Image_71,
+    Image_72,
+    Image_73,
+    Image_74,
+    Image_75,
+    Image_76,
+    Image_77,
+    Image_78,
+    Image_79,
+    Image_80,
+    Image_81,
+    Image_82,
+    Image_83,
+    Image_84,
+    Image_85,
+    Image_86,
+    Image_87,
+    Image_88,
+    Image_89,
+    Image_90,
+    Image_91,
+    Image_92,
+    Image_93,
+    Image_94,
+    Image_95,
+    Image_96,
+    Image_97,
+    Image_98,
+    Image_99,
+    Image_100,
+    Image_101,
+    Image_102,
+    Image_103,
+    Image_104,
+    Image_105,
+    Image_106,
+    Image_107,
+    Image_108,
+    Image_109,
+    Image_110,
+    Image_111,
+    Image_112,
+    Image_113,
+    Image_114,
+    Image_115,
+    Image_116,
+    Image_117,
+    Image_118,
+    Image_119,
+    Image_120,
+    Image_121,
+    Image_122,
+    Image_123,
+    Image_124,
+    Image_125,
+    Image_126,
+    Image_127,
+    Image_128,
+    Image_129,
+    Image_130,
+    Image_131,
+    Image_132,
+    Image_133,
+    Image_134,
+    Image_135,
+    Image_136,
+    Image_137,
+    Image_138,
+    Image_139,
+    Image_140,
+    Image_141,
+    Image_142,
+    Image_143,
+    Image_144,
+    Image_145,
+    Image_146,
+    Image_147,
+    Image_148,
+    Image_149,
+    Image_150,
+    Image_151,
+    Image_152,
+    Image_153,
+    Image_154,
+    Image_155,
+    Image_156,
+    Image_157,
+    Image_158,
+    Image_159,
+    Image_160,
+    Image_161,
+    Image_162,
+    Image_163,
+    Image_164,
+    Image_165,
+    Image_166,
+    Image_167,
+    Image_168,
+    Image_169,
+    Image_170,
+    Image_171,
+    Image_172,
+    Image_173,
+    Image_174,
+    Image_175,
+    Image_176,
+    Image_177,
+    Image_178,
+    Image_179,
+)
---
+++ public/potrait/effects/blackToWhite/index.js
@@ -0,0 +1,91 @@
+import Image_1 from './black_white_transition_1.png'
+import Image_2 from './black_white_transition_2.png'
+import Image_3 from './black_white_transition_3.png'
+import Image_4 from './black_white_transition_4.png'
+import Image_5 from './black_white_transition_5.png'
+import Image_6 from './black_white_transition_6.png'
+import Image_7 from './black_white_transition_7.png'
+import Image_8 from './black_white_transition_8.png'
+import Image_9 from './black_white_transition_9.png'
+import Image_10 from './black_white_transition_10.png'
+import Image_11 from './black_white_transition_11.png'
+import Image_12 from './black_white_transition_12.png'
+import Image_13 from './black_white_transition_13.png'
+import Image_14 from './black_white_transition_14.png'
+import Image_15 from './black_white_transition_15.png'
+import Image_16 from './black_white_transition_16.png'
+import Image_17 from './black_white_transition_17.png'
+import Image_18 from './black_white_transition_18.png'
+import Image_19 from './black_white_transition_19.png'
+import Image_20 from './black_white_transition_20.png'
+import Image_21 from './black_white_transition_21.png'
+import Image_22 from './black_white_transition_22.png'
+import Image_23 from './black_white_transition_23.png'
+import Image_24 from './black_white_transition_24.png'
+import Image_25 from './black_white_transition_25.png'
+import Image_26 from './black_white_transition_26.png'
+import Image_27 from './black_white_transition_27.png'
+import Image_28 from './black_white_transition_28.png'
+import Image_29 from './black_white_transition_29.png'
+import Image_30 from './black_white_transition_30.png'
+import Image_31 from './black_white_transition_31.png'
+import Image_32 from './black_white_transition_32.png'
+import Image_33 from './black_white_transition_33.png'
+import Image_34 from './black_white_transition_34.png'
+import Image_35 from './black_white_transition_35.png'
+import Image_36 from './black_white_transition_36.png'
+import Image_37 from './black_white_transition_37.png'
+import Image_38 from './black_white_transition_38.png'
+import Image_39 from './black_white_transition_39.png'
+import Image_40 from './black_white_transition_40.png'
+import Image_41 from './black_white_transition_41.png'
+import Image_42 from './black_white_transition_42.png'
+import Image_43 from './black_white_transition_43.png'
+import Image_44 from './black_white_transition_44.png'
+
+export const Images = Array.of(
+    Image_1,
+    Image_2,
+    Image_3,
+    Image_4,
+    Image_5,
+    Image_6,
+    Image_7,
+    Image_8,
+    Image_9,
+    Image_10,
+    Image_11,
+    Image_12,
+    Image_13,
+    Image_14,
+    Image_15,
+    Image_16,
+    Image_17,
+    Image_18,
+    Image_19,
+    Image_20,
+    Image_21,
+    Image_22,
+    Image_23,
+    Image_24,
+    Image_25,
+    Image_26,
+    Image_27,
+    Image_28,
+    Image_29,
+    Image_30,
+    Image_31,
+    Image_32,
+    Image_33,
+    Image_34,
+    Image_35,
+    Image_36,
+    Image_37,
+    Image_38,
+    Image_39,
+    Image_40,
+    Image_41,
+    Image_42,
+    Image_43,
+    Image_44,
+)
